7554973311f8a26bd20c2be68e9ba7d652e6d36d
Seba Scr <sebaq3@gmail.com>
2023-06-11 14:15:42 -0300

    fer merge

--- proyecto_actual/src/layouts/GeneralLayout.jsx
+++ proyecto_actual/src/layouts/GeneralLayout.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
 
+
 const GeneralLayout = ({children}) => {
   return (
     <div><Navbar>
         </Navbar>
+        
         {children}
     </div>
   )
