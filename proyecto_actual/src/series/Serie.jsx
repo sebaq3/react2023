61ff12855ccf25521a99c418272ea7808235b5e7
gpieruzzini <gpieruzzini@gmail>
2023-06-26 16:24:22 -0300

    con custom hook

--- proyecto_actual/src/series/Serie.jsx
+++ proyecto_actual/src/series/Serie.jsx
@@ -1,58 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Card from './Card';
 import GeneralLayout from '../layouts/GeneralLayout';
+import useSeries from '../hooks/useSeries';
 
 const Serie = () => {
-  const [series, setSeries] = useState([]);
-  const [genres, setGenres] = useState({});
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(0);
 
-  useEffect(() => {
-    const fetchGenres = async () => {
-      try {
-        const response = await fetch(
-          `https://api.themoviedb.org/3/genre/tv/list?api_key=e978d37cf754ee2def0362a69807b215`
-        );
-        const data = await response.json();
-        const genreMap = {};
-        data.genres.forEach((genre) => {
-          genreMap[genre.id] = genre.name;
-        });
-        setGenres(genreMap);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+  const {series, genres, currentPage, totalPages, handlePrevPage, handleNextPage} = useSeries()
 
-    const fetchSeries = async () => {
-      try {
-        const response = await fetch(
-          `https://api.themoviedb.org/3/tv/popular?api_key=e978d37cf754ee2def0362a69807b215&page=${currentPage}`
-        );
-        const data = await response.json();
-        setSeries(data.results);
-        setTotalPages(data.total_pages);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchGenres();
-    fetchSeries();
-  }, [currentPage]);
-
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1);
-    }
-  };
-
-  const handlePrevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  };
 
   return (
     <div>
